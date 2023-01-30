df339aea3fdfd6d41c94ab93b0ba42c8f8b671b6
Michael Kennedy <michaelkennedy_228@hotmail.com>
2023-01-30 00:02:35 -0400

    Added code for the databse tables and added docstring comments

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    """Database table for Questions."""
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    """Database table for Question choices."""
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
